7139435866a6e2b99ce72e873717476a0839d50c
Sunil Wananje <sunil.wananje@reisystems.in>
2019-02-01 17:51:54 +0530

    css path updated for login page

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -13,16 +13,16 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
-    <link href="{{asset('assets/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/AdminPanel/plugins/bootstrap/css/bootstrap.css')}}" rel="stylesheet">
 
     <!-- Waves Effect Css -->
-    <link href="{{asset('assets/plugins/node-waves/waves.css')}}" rel="stylesheet" />
+    <link href="{{asset('vendor/AdminPanel/plugins/node-waves/waves.css')}}" rel="stylesheet" />
 
     <!-- Animation Css -->
-    <link href="{{asset('assets/plugins/animate-css/animate.css')}}" rel="stylesheet" />
+    <link href="{{asset('vendor/AdminPanel/plugins/animate-css/animate.css')}}" rel="stylesheet" />
 
     <!-- Custom Css -->
-    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('vendor/AdminPanel/css/style.css')}}" rel="stylesheet">
 </head>
 
 <body class="login-page">
@@ -74,20 +74,20 @@
     </div>
 
     <!-- Jquery Core Js -->
-    <script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/plugins/jquery/jquery.min.js')}}"></script>
 
     <!-- Bootstrap Core Js -->
-    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/plugins/bootstrap/js/bootstrap.js')}}"></script>
 
     <!-- Waves Effect Plugin Js -->
-    <script src="{{asset('assets/plugins/node-waves/waves.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/plugins/node-waves/waves.js')}}"></script>
 
     <!-- Validation Plugin Js -->
-    <script src="{{asset('assets/plugins/jquery-validation/jquery.validate.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/plugins/jquery-validation/jquery.validate.js')}}"></script>
 
     <!-- Custom Js -->
-    <script src="{{asset('assets/js/admin.js')}}"></script>
-    <script src="{{asset('assets/js/pages/examples/sign-in.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/js/admin.js')}}"></script>
+    <script src="{{asset('vendor/AdminPanel/js/pages/examples/sign-in.js')}}"></script>
 </body>
 
 </html>
