70be15cade960b533dbdbd60f3b1bf9f44fe7c95
Sunil Wananje <sunil.wananje@reisystems.in>
2019-02-01 18:03:40 +0530

    login form action url updated

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -33,14 +33,14 @@
         </div>
         <div class="card">
             <div class="body">
-                <form id="sign_in" method="POST">
+                <form id="sign_in" method="GET" action="{{ route('admin.dashboard') }}">
                     <div class="msg">Sign in to start your session</div>
                     <div class="input-group">
                         <span class="input-group-addon">
                             <i class="material-icons">person</i>
                         </span>
                         <div class="form-line">
-                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                            <input type="text" class="form-control" name="username" placeholder="Username" autofocus>
                         </div>
                     </div>
                     <div class="input-group">
@@ -48,7 +48,7 @@
                             <i class="material-icons">lock</i>
                         </span>
                         <div class="form-line">
-                            <input type="password" class="form-control" name="password" placeholder="Password" required>
+                            <input type="password" class="form-control" name="password" placeholder="Password">
                         </div>
                     </div>
                     <div class="row">
